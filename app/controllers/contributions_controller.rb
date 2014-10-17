eb1e300b29cbfe6a588b187073e00c4e0803a6e0
Lauren Fazah <laurenfazah@gmail.com>
2014-10-17 14:56:08 -0400

    created methods for contributions controller

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,4 +1,31 @@
 class ContributionsController < ApplicationController
 
 	before_action :authenticate_user!, except: [:show, :index]
+
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def edit
+	end
+
+	def destroy
+	end
+
+	private
+	def contribution_params
+		params.require(:contribution).permit(:contribution)
+	end
 end
