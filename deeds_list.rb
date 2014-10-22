ced990312bb1511ad4f3c6584e7411d0bdb4aea2
Lauren Fazah <laurenfazah@gmail.com>
2014-10-22 14:06:31 -0400

    changed heroku run rake task command from scheduler

---
+++ deeds_list.rb
@@ -0,0 +1,18 @@
+[ 
+	# "Help someone with a task they're struggling with.",
+	# "Let someone know you appreciate them.",
+	# "Share a smile - they're contagious.",
+	# "Bring in a treat for a classmate or coworker just because.",
+	# "Pay for the coffee of the customer behind you in line.",
+	# "Ask someone out for lunch you know but haven't gotten to know yet.",
+	"Ask to hear more from someone about something you know they're passionate about.",
+	"Give a stranger a friendly compliment.",
+	"Write a personal note to your waiter or waitress thanking them for their service.",
+	"This one is easy - be friendly!"
+	"If you can say yes, say yes."
+
+
+
+
+
+]
