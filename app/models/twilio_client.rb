e5343a0136b4984c3362cdeb04bc503f4fc1d3cb
Lauren Fazah <laurenfazah@gmail.com>
2014-10-23 11:01:43 -0400

    added planning files to app/assets/images folder

--- app/models/twilio_client.rb
+++ app/models/twilio_client.rb
@@ -26,7 +26,14 @@ class TwilioClient
 	"Share a smile - they're contagious.",
 	"Bring in a treat for a classmate or coworker just because.",
 	"Pay for the coffee of the customer behind you in line.",
-	"Ask someone out for lunch you know but haven't gotten to know yet."].sample
+	"Ask someone out for lunch you know but haven't gotten to know yet.",
+	"Ask to hear more from someone about something you know they're passionate about.",
+	"Give a stranger a friendly compliment.",
+	"Write a personal note to your waiter or waitress thanking them for their service.",
+	"This one is easy - be friendly!",
+	"If you can say yes, say yes.",
+	"Volunteer for a community service event.",
+	"If you don't have a pet allergy, go to your local pet shelter and give those cats and dogs (and others) some attention.",].sample
 	end
 
 	def all_phone_numbers
--- deeds_list.rb
+++ deeds_list.rb
@@ -5,11 +5,14 @@
 	# "Bring in a treat for a classmate or coworker just because.",
 	# "Pay for the coffee of the customer behind you in line.",
 	# "Ask someone out for lunch you know but haven't gotten to know yet.",
-	"Ask to hear more from someone about something you know they're passionate about.",
-	"Give a stranger a friendly compliment.",
-	"Write a personal note to your waiter or waitress thanking them for their service.",
-	"This one is easy - be friendly!"
-	"If you can say yes, say yes."
+	# "Ask to hear more from someone about something you know they're passionate about.",
+	# "Give a stranger a friendly compliment.",
+	# "Write a personal note to your waiter or waitress thanking them for their service.",
+	# "This one is easy - be friendly!",
+	# "If you can say yes, say yes.",
+	# "Volunteer for a community service event.",
+	# "If you don't have a pet allergy, go to your local pet shelter and give those cats and dogs (and others) some attention.",
+	
 
 
 
