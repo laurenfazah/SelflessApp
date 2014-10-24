a3fead5e9aa9ed64b38da412a23a9555c38cbca6
Lauren Fazah <laurenfazah@gmail.com>
2014-10-23 20:14:53 -0400

    moved notes from readme

---
+++ Notes.rb
@@ -0,0 +1,34 @@
+
+
+Notes: 
+models - (don't inherit) TwilioClients
+	class method: sets up and sends
+
+	can call from any controller or model
+
+	active record callbacks/hooks (after save) - can say to run method 
+	after save
+
+	add columns to devise
+	alter in app controller
+	in devise readme  - for strong params
+
+Find Online:
+	http://selfless-app.com
+
+
+Presentation notes:
+
+I like simple, so I wanted to make something simple and effective. Hence, Selfless.
+	planning the project
+	building
+	what it does
+	how it works
+	sign up
+
+To Do in Future:
+	change message method
+	add international note for signing up 
+	add my name in footer
+
+
